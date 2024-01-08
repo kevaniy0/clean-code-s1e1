216fac2cc10ddeaafb855237a6a573dc23194e9c
kevaniy0 <sevak_08@mail.ru>
2024-01-08 07:33:37 +0300

    refactor(basic-3.2): add meaningful name for class names

--- app.js
+++ app.js
@@ -77,10 +77,10 @@ var editTask = function () {
   var editInput = listItem.querySelector("input[type=text]");
   var label = listItem.querySelector("label");
   var editBtn = listItem.querySelector(".edit");
-  var containsClass = listItem.classList.contains("edit-mode");
-  //If class of the parent is .edit-mode
+  var containsClass = listItem.classList.contains("edit-task");
+  //If class of the parent is .edit-task
   if (containsClass) {
-    //switch to .edit-mode
+    //switch to .edit-task
     //label becomes the inputs value.
     label.innerText = editInput.value;
     editBtn.innerText = "Edit";
@@ -89,8 +89,8 @@ var editTask = function () {
     editBtn.innerText = "Save";
   }
 
-  //toggle .edit-mode on the parent.
-  listItem.classList.toggle("edit-mode");
+  //toggle .edit-task on the parent.
+  listItem.classList.toggle("edit-task");
 };
 
 //Delete task.
